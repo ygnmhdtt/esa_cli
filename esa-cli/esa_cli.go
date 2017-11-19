a3a270658a4a935cf8a068c36bbbd8c94b2fdf2c
Hidetatsu Yaginuma <yaginuma@mmmcorp.co.jp>
2017-11-19 13:44:49 +0900

    add team function

--- esa-cli/esa_cli.go
+++ esa-cli/esa_cli.go
@@ -12,11 +12,48 @@ import (
 	"time"
 )
 
-type Client struct {
-	URLV1         *url.URL
+type Client_V1 struct {
+	URL           *url.URL
+	TeamName      string
 	HTTPClient    *http.Client
 	Authorization string
 	Logger        *log.Logger
+}
+
+type Team struct {
+	Name        string `json:"name"`
+	Privacy     string `json:"privacy"`
+	Description string `json:"description"`
+	Icon        string `json:"icon"`
+	URL         string `json:"url"`
+}
+
+type TeamStats struct {
+	Members            int `json:"members"`
+	Posts              int `json:"posts"`
+	PostsWip           int `json:"posts_wip"`
+	PostsShipped       int `json:"posts_shipped"`
+	Comments           int `json:"comments"`
+	Stars              int `json:"stars"`
+	DailyActiveUsers   int `json:"daily_active_users"`
+	WeeklyActiveUsers  int `json:"weekly_active_users"`
+	MonthlyActiveUsers int `json:"monthly_active_users"`
+}
+
+type TeamMembers struct {
+	Members []struct {
+		Name       string `json:"name"`
+		ScreenName string `json:"screen_name"`
+		Icon       string `json:"icon"`
+		Email      string `json:"email"`
+		PostsCount int    `json:"posts_count"`
+	} `json:"members"`
+	PrevPage   interface{} `json:"prev_page"`
+	NextPage   interface{} `json:"next_page"`
+	TotalCount int         `json:"total_count"`
+	Page       int         `json:"page"`
+	PerPage    int         `json:"per_page"`
+	MaxPerPage int         `json:"max_per_page"`
 }
 
 type Post struct {
@@ -59,19 +96,25 @@ func decodeBody(resp *http.Response, out interface{}) error {
 	return decoder.Decode(out)
 }
 
-func NewClient(auth string) *Client {
-	client := new(Client)
-	u, _ := url.Parse("https://api.esa.io/v1")
-	client.URLV1 = u
+func NewClient(auth string, teamName string) *Client_V1 {
+	client := new(Client_V1)
+	if os.Getenv("TEST") == "1" {
+		u, _ := url.Parse(os.Getenv("TEST_URL"))
+		client.URL = u
+	} else {
+		u, _ := url.Parse("https://api.esa.io/v1")
+		client.URL = u
+	}
+	client.TeamName = teamName
 	client.HTTPClient = &http.Client{}
 	client.Authorization = auth
 	client.Logger = log.New(os.Stdout, "logger: ", log.Lshortfile)
 	return client
 }
 
-func (c *Client) newRequest(method string, spath string, body io.Reader) (*http.Request, error) {
-	u := *c.URLV1
-	u.Path = path.Join(c.URLV1.Path, spath)
+func (c *Client_V1) newRequest(method string, spath string, body io.Reader) (*http.Request, error) {
+	u := *c.URL
+	u.Path = path.Join(c.URL.Path, spath)
 	req, err := http.NewRequest(method, u.String(), body)
 	if err != nil {
 		return nil, err
@@ -80,7 +123,52 @@ func (c *Client) newRequest(method string, spath string, body io.Reader) (*http.
 	return req, nil
 }
 
-func (c *Client) GetPost(id string) (*Post, error) {
+func (c *Client_V1) GetTeam() (*Team, error) {
+	spath := fmt.Sprintf("/teams/%v", c.TeamName)
+	req, _ := c.newRequest("GET", spath, nil)
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	var team Team
+	if err := decodeBody(res, &team); err != nil {
+		return nil, err
+	}
+	return &team, err
+}
+
+func (c *Client_V1) GetTeamStats() (*TeamStats, error) {
+	spath := fmt.Sprintf("/teams/%v/stats", c.TeamName)
+	req, _ := c.newRequest("GET", spath, nil)
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	var teamStats TeamStats
+	if err := decodeBody(res, &teamStats); err != nil {
+		return nil, err
+	}
+	return &teamStats, err
+}
+
+func (c *Client_V1) GetTeamMembers(page int) (*TeamMembers, error) {
+	spath := fmt.Sprintf("/teams/%v/members?page=%v", c.TeamName, page)
+	req, _ := c.newRequest("GET", spath, nil)
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	var teamMembers TeamMembers
+	if err := decodeBody(res, &teamMembers); err != nil {
+		return nil, err
+	}
+	return &teamMembers, err
+}
+
+func (c *Client_V1) GetPost(id string) (*Post, error) {
 	spath := fmt.Sprintf("/teams/mmmcorp/posts/%v", id)
 	req, _ := c.newRequest("GET", spath, nil)
 	res, err := c.HTTPClient.Do(req)
